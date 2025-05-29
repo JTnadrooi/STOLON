b9ea10fa4b14b79497af60798eac5539f0a3a3e3
JTnadrooi <jtnadrooi@gmail.com>
2025-05-29 16:00:55 +0200

    added basic "EntitySelectGameState" composition logic

--- src/Components/GameState/EntitySelectGameState.cs
+++ src/Components/GameState/EntitySelectGameState.cs
@@ -24,7 +24,13 @@ namespace STOLON
         private int _line2x;
         private Tweener<float> _lineTweener;
 
+        private bool _initDone;
+
+        private List<Vector2> _tilePositions;
+
         public const int TILE_SIZE = 128;
+        public const int TILE_ROW_AMOUNT = 4;
+        public const int TILE_COLUMN_AMOUNT = 2;
 
         public EntitySelectGameState()
         {
@@ -32,23 +38,38 @@ namespace STOLON
             STOLON.StateManager.TryGetState(out _menuGameState!);
             _lineTweener = new Tweener<float>(0, 1, 2, Ease.Quad.InOut);
             _lineTweener.Start();
+
+            _tilePositions = new List<Vector2>();
+            for (int i = 0; i < 8; i++)
+            {
+                _tilePositions.Add(new Vector2(i % TILE_ROW_AMOUNT * TILE_SIZE, i / TILE_ROW_AMOUNT * TILE_SIZE + 32));
+            }
         }
 
         public void Update(int elapsedMilliseconds)
         {
+            int To(int orgin, int target, float amount) => (int)(orgin + (target - orgin) * amount);
             _lineTweener.Update(elapsedMilliseconds / 1000f);
-            int menuLineRemoveDelta = TILE_SIZE * 2 - _menuGameState.MenuRemoveLine1x;
-            int lineOffset = (int)(_menuGameState.MenuRemoveLine1x + menuLineRemoveDelta * _lineTweener.Value);
+            _initDone = !_lineTweener.Running;
+            //int line1Target = STOLON.Instance.VirtualDimensions.X - (_menuGameState.MenuRemoveLine2x - _menuGameState.MenuRemoveLine1x) + 144;
+            int line1Target = TILE_SIZE * 4;
+            int line2Target = STOLON.Instance.VirtualDimensions.X - 32;
 
-            _line1x = lineOffset;
-            _line2x = STOLON.Instance.VirtualDimensions.X - lineOffset;
+            _line1x = To(_menuGameState.MenuRemoveLine1x, line1Target, _lineTweener.Value);
+            _line2x = To(_menuGameState.MenuRemoveLine2x, line2Target, _lineTweener.Value);
         }
 
         public void Draw(SpriteBatch spriteBatch, int elapsedMiliseconds)
         {
-            spriteBatch.Draw(_tileTexture, Vector2.Zero, Color.White);
-            spriteBatch.Draw(_tileTexture, new Vector2(TILE_SIZE, 0), Color.White);
-            spriteBatch.Draw(STOLON.Textures.GetReference("textures\\characters\\silo"), new Vector2(500, 0), Color.White);
+            if (_initDone)
+            {
+                spriteBatch.Draw(STOLON.Textures.GetReference("textures\\characters\\silo"), new Vector2(448, 0), Color.White);
+                spriteBatch.Draw(STOLON.Textures.Pixel, new Rectangle(0, 0, _line1x, 1000), Color.Black);
+                foreach (Vector2 tilePos in _tilePositions)
+                {
+                    spriteBatch.Draw(_tileTexture, tilePos, Color.White);
+                }
+            }
             spriteBatch.DrawLine(_line1x, -10f, _line1x, 1000f, Color.White, UserInterface.LINE_WIDTH);
             spriteBatch.DrawLine(_line2x, -10f, _line2x, 1000f, Color.White, UserInterface.LINE_WIDTH);
         }
