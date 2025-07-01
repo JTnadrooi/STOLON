9711f2db5a9f8f3d6218b7968724b6129c99ca35
JTnadrooi <jtnadrooi@gmail.com>
2025-07-01 16:07:32 +0200

    reorder Graphic structures to seperate files

---
+++ src/Components/UI/Graphic.cs
@@ -0,0 +1,17 @@
+﻿using AsitLib;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Frozen;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using static STOLON.UIElement;
+
+namespace STOLON
+{
+    public interface IGraphic
+    {
+        void Draw(DrawingContext drawingContext);
+    }
+
+}
