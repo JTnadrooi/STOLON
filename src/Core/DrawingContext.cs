2be3feb0874465b605b206ca8349d7e4b44a58e1
JTnadrooi <jtnadrooi@gmail.com>
2025-07-02 19:42:10 +0200

    fix: Hide not having any effect

--- src/Core/DrawingContext.cs
+++ src/Core/DrawingContext.cs
@@ -240,6 +240,7 @@ namespace STOLON
         public void DrawElement(UIElementDrawData drawData)
         {
             if (drawData.Id == null) throw new InvalidOperationException();
+            if (drawData.Hide) return;
             DrawString(drawData.Font, drawData.Text, drawData.Position);
             if (drawData.DrawRectangle) DrawRectangle(drawData.Rectangle, Color.White, 1f);
         }
