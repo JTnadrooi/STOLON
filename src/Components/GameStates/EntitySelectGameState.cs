b3caf801ae2dbe35f072bbef63909ab1db30d2bd
JTnadrooi <jtnadrooi@gmail.com>
2025-07-23 17:47:34 +0200

    add connectionLine draw when secondary SymbolNotation hover

--- src/Components/GameStates/EntitySelectGameState.cs
+++ src/Components/GameStates/EntitySelectGameState.cs
@@ -271,7 +271,7 @@ namespace STOLON
             {
                 int entityIndex = _selection[slotIndex];
                 if (entityIndex == -1) continue;
-                if (_hoveredIndex == entityIndex)
+                if (_hoveredIndex == entityIndex || _drawAllocationDataDump[slotIndex]!.Value.SymbolNotationRect.Contains(STOLON.Input.VirtualMousePos))
                 {
                     _drawConnectionLine = true;
                     _connectionLine = new Line(_entityDrawDump[entityIndex].Pos.ToPoint() + new Point(TILE_SIZE / 2, 0), _drawAllocationDataDump[GetSlot(entityIndex)]!.Value.SymbolNotationRect.Location + new Point(SYMBOL_NOTATION_SIZE / 2, SYMBOL_NOTATION_SIZE));
