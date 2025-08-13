707eae617f888abf021fab76ca22bc282c6f7345
JTnadrooi <jtnadrooi@gmail.com>
2025-08-13 19:44:47 +0200

    add Entity constructor default values

--- src/Entities/Entity.cs
+++ src/Entities/Entity.cs
@@ -81,15 +81,15 @@ namespace STOLON
 
         public const string DEBUG_DESC = "In the Etruscan language, plosive consonants had no contrastive voicing, so the Greek '' (Gamma) was adopted into the Etruscan alphabet to represent /k/. Already in the Western Greek alphabet, Gamma first took a '' form in Early Etruscan, then '' in Classical Etruscan. In Latin, it eventually took the 'c' form in Classical Latin. In the earliest Latin inscriptions, the letters 'c k q' were used to represent the sounds /k/ and // (which were not differentiated in writing). Of these, 'q' was used to represent /k/ or // before a rounded vowel, 'k' before 'a', and 'c' elsewhere.[3] During the 3rd century BC, a modified character was introduced for //, and 'c' itself was retained for /k/. The use of 'c' (and its variant 'g') replaced most usages of 'k' and 'q'. Hence, in the classical period and after, 'g' was treated as the equivalent of Greek gamma, and 'c' as the equivalent of kappa; this shows in the romanization of Greek words, as in '', '', and '' came into Latin as 'cadmvs', 'cyrvs' and 'phocis', respectively.";
 
-        public Entity(string id, string name, string symbolNotation, EntityProfile profile, ConditionalNote[] notes, string description, string? fullName = null)
+        public Entity(string id, string name, string symbolNotation, EntityProfile profile, ConditionalNote[]? notes = null, string? description = null, string? fullName = null)
         {
             Id = id;
             Name = name;
             SymbolNotation = symbolNotation;
             Profile = profile ?? new EntityProfile(id);
             FullName = fullName ?? name;
-            Description = description;
-            Notes = notes;
+            Description = description ?? string.Empty;
+            Notes = notes ?? Array.Empty<ConditionalNote>();
         }
         /// <summary>
         /// Get the <see cref="Player"/> of this <see cref="Entity"/>.
