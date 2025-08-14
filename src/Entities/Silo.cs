54d3d6d19817c8882f9cf75d92bac0d974ca9442
JTnadrooi <jtnadrooi@gmail.com>
2025-08-14 17:43:24 +0200

    minor wording cleanup

--- src/Entities/Silo.cs
+++ src/Entities/Silo.cs
@@ -22,17 +22,14 @@ namespace STOLON
     {
         public SiloEntity() : base("silo", "Silo", "Sl", new EntityProfile("silo", new Point(245, 180)),
             [
-                new ConditionalNote("Gains 20% allocation when Deceit is selected.", i => i.IsSelected("deceit"), ConditionalNotePolarity.Positive),
-                new ConditionalNote("Loses 50% allocation when more than 3 entities are selected.", i => i.Entries.Count > 3, ConditionalNotePolarity.Positive),
+                new ConditionalNote("Gains 20% valloc when Deceit is selected.", i => i.IsSelected("deceit"), ConditionalNotePolarity.Positive),
+                new ConditionalNote("Loses 50% valloc when more than 3 entities are selected.", i => i.Entries.Count > 3, ConditionalNotePolarity.Positive),
             ],
             [
                 new ConditionalNote("If valloc is above 50%, gain the abilty to decide where the opponent places their marker. You will not be able to win in one of the 3 moves after.", i => i.GetVirtualAllocation("silo") > 50, ConditionalNotePolarity.Positive),
                 new ConditionalNote("If valloc is above 20%, gain the abilty to remove oppenent markers from a row, must be normal gravity.", i => i.GetVirtualAllocation("silo") > 20, ConditionalNotePolarity.Positive),
-
             ], "Silo", "Silo 28SHA")
-        {
-        }
-
+        { }
         public override int GetVirtualAllocation(SelectionInfo info)
         {
             return new AllocationHelperChain(info, this)
