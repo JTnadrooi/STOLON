510e36ea5d82397ce273353045500dc0f191021e
JTnadrooi <jtnadrooi@gmail.com>
2025-08-25 08:56:22 +0200

    remove misleading valloc percentages in silo's 2nd note array

--- src/Entities/Silo.cs
+++ src/Entities/Silo.cs
@@ -26,8 +26,8 @@ namespace STOLON
                 new ConditionalNote("Loses 50% valloc when more than 3 entities are selected.", i => i.Entries.Count > 3, ConditionalNotePolarity.Negative),
             ],
             [
-                new ConditionalNote("If valloc is above 50%, gain the abilty to decide where the opponent places their marker. You will not be able to win in one of the 3 moves after.", i => i.GetVirtualAllocation("silo") > 50, ConditionalNotePolarity.Positive),
-                new ConditionalNote("If valloc is above 20%, gain the abilty to remove oppenent markers from a row, must be normal gravity.", i => i.GetVirtualAllocation("silo") > 20, ConditionalNotePolarity.Positive),
+                new ConditionalNote("If valloc is above 50, gain the abilty to decide where the opponent places their marker. You will not be able to win in one of the 3 moves after.", i => i.GetVirtualAllocation("silo") > 50, ConditionalNotePolarity.Positive),
+                new ConditionalNote("If valloc is above 20, gain the abilty to remove oppenent markers from a row, must be normal gravity.", i => i.GetVirtualAllocation("silo") > 20, ConditionalNotePolarity.Positive),
             ], "Silo", "Silo 28SHA")
         { }
         public override int GetVirtualAllocation(EntitySelection info)
