6b008d5f44bd2a137246841c943f1a08d40ee7de
JTnadrooi <jtnadrooi@gmail.com>
2025-09-10 18:52:45 +0200

    overhaul file structure (seperate CSPROJ folders)

--- src/STOLON.Installer/Program.cs
+++ src/STOLON.Installer/Program.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AsitLib.Debug;
 
-namespace STOLONInstaller
+namespace STOLON.Installer
 {
     public class Program
     {
