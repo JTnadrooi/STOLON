6fb298abf06d9db9119a78c6aa8787ea4271bf45
JTnadrooi <jtnadrooi@gmail.com>
2025-09-10 18:17:49 +0200

    add STOLON.Installer

---
+++ STOLONInstaller/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STOLONInstaller
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(2);
+            Console.ReadLine();
+        }
+    }
+}
