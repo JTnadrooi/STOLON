629a47d554f143b04ac034e086bb36814e5be966
JTnadrooi <jtnadrooi@gmail.com>
2025-09-14 19:53:35 +0200

    keep program running after failed command, unless DEBUG_MODE = true

--- src/STOLON.Cmd/Program.cs
+++ src/STOLON.Cmd/Program.cs
@@ -1,21 +1,13 @@
 ﻿using AsitLib;
-using AsitLib.Debug;
 using STOLON.Cmd;
-using System;
-using System.Collections.Frozen;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
+#pragma warning disable CS0162 // Unreachable code detected
 
 namespace STOLON.Installer
 {
     public static class Program
     {
+        public const bool DEBUG_MODE = false;
+
         public static void Main(string[] args)
         {
             using CommandHandler cmdHandler = new CommandHandler(args.Contains("-s"));
@@ -27,14 +19,17 @@ namespace STOLON.Installer
                 {
                     Console.Write("> ");
                     string[] newArgs = CommandHelpers.SplitArgs(Console.ReadLine()!);
-                    try
-                    {
+                    if (DEBUG_MODE)
                         CommandHandler.Instance.Execute(newArgs);
-                    }
-                    catch (Exception e)
-                    {
-                        CommandHandler.Instance.Debug.Log("command failed: " + e.Message);
-                    }
+                    else
+                        try
+                        {
+                            CommandHandler.Instance.Execute(newArgs);
+                        }
+                        catch (Exception e)
+                        {
+                            CommandHandler.Instance.Debug.Log("command failed: " + e.Message);
+                        }
                 }
             }
             else CommandHandler.Instance.Execute(args);
