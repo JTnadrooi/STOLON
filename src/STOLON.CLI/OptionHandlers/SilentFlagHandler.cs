44c1887be2426c82b81b8db49f0700df05ddc1b1
JTnadrooi <jtnadrooi@gmail.com>
2025-09-19 21:02:35 +0200

    add SilentFlagHandler (fix VerboseFlagHandler name)

---
+++ src/STOLON.CLI/OptionHandlers/SilentFlagHandler.cs
@@ -0,0 +1,29 @@
+﻿using STOLON.CLI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static STOLON.CLI.CommandHelpers;
+
+namespace STOLON.CLI
+{
+    public class SilentFlagHandler : FlagHandler
+    {
+        private TextWriter? originalOut;
+
+        public SilentFlagHandler() : base("silent", "Silences stdout.", "s")
+        {
+            originalOut = null;
+        }
+
+        public override void PreCommand(ArgumentsInfo arguments)
+        {
+            originalOut = Console.Out;
+            Console.SetOut(TextWriter.Null);
+        }
+
+        public override void PostCommand() => Console.SetOut(originalOut!);
+    }
+}
