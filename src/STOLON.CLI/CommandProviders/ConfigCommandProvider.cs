1350a13347da34c19ff83264c2fa4d27efe22351
JTnadrooi <jtnadrooi@gmail.com>
2025-09-20 15:06:39 +0200

    add ConfigCommandProvider; 1 command

---
+++ src/STOLON.CLI/CommandProviders/ConfigCommandProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STOLON.CLI
+{
+    public class ConfigCommandProvider : CommandProvider
+    {
+        public ConfigCommandProvider() : base("conf") { }
+
+        [Command("Open user.ini file.")]
+        public void Open()
+        {
+            Process.Start("notepad.exe", "user.ini");
+            Console.WriteLine("Opened user config file (user.ini).");
+        }
+    }
+}
