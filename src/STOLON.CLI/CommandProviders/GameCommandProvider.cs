b52656957c10ca63bd930f7665c5e15c23778297
JTnadrooi <jtnadrooi@gmail.com>
2025-09-20 13:04:06 +0200

    add "start" command

---
+++ src/STOLON.CLI/CommandProviders/GameCommandProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STOLON.CLI
+{
+    public class GameCommandProvider : CommandProvider
+    {
+        public GameCommandProvider() : base("sl")
+        {
+
+        }
+        [Command("Open the folder where STOLON is located.", useProviderNamespace: false)]
+        public void Start()
+        {
+            Process.Start("STOLON.exe");
+            Console.WriteLine("Started STOLON.");
+        }
+    }
+}
