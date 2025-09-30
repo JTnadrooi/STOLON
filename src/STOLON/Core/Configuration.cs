715dae49642d6d4ea11ea972a40f7879120c3e85
JTnadrooi <jtnadrooi@gmail.com>
2025-09-30 21:26:01 +0200

    rename conf key "resolution" to "res"

--- src/STOLON/Core/Configuration.cs
+++ src/STOLON/Core/Configuration.cs
@@ -81,8 +81,8 @@ namespace STOLON
                 {"graphics.entities_show_on_menu", true},
                 {"graphics.splashtexts_show", true},
                 {"graphics.fullscreen", false},
-                {"graphics.resolution.w", 1920},
-                {"graphics.resolution.h", 1080},
+                {"graphics.res.w", 1920},
+                {"graphics.res.h", 1080},
                 {"graphics.crt.enable", true},
 
                 {"cli.catch_errors", true},
