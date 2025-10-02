c5c6f0b22ba0e8340c296519eb82b9079c1cc561
JTnadrooi <jtnadrooi@gmail.com>
2025-10-02 20:39:28 +0200

    add ThemeCommandProvider

---
+++ src/STOLON.CLI/CommandProviders/ThemeCommandProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STOLON.CLI
+{
+    public class ThemeCommandProvider : CommandProvider
+    {
+        public ThemeCommandProvider() : base("theme")
+        {
+
+        }
+
+        [Command("Print the name of the color theme.", isReadOnly: true)]
+        public void _M()
+        {
+            Console.WriteLine(CLI.Instance.Config.Get<string>("graphics.theme"));
+        }
+
+        [Command("Set the color theme.")]
+        public void Set(string themeId)
+        {
+            CLI.Instance.Config.Set("graphics.theme", themeId);
+        }
+
+        [Command("Set the color theme.")]
+        public void Reset(string themeId)
+        {
+            CLI.Instance.Config.Reset("graphics.theme");
+        }
+
+        [Command("Print the path of the color theme.")]
+        public void Path(string themeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
