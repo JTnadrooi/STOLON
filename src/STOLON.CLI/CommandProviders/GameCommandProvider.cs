3abe49c81e122033b0fb08bc98d992b907ad5be1
JTnadrooi <jtnadrooi@gmail.com>
2025-10-19 20:37:29 +0200

    Fix SetVersion command

--- src/STOLON.CLI/CommandProviders/GameCommandProvider.cs
+++ src/STOLON.CLI/CommandProviders/GameCommandProvider.cs
@@ -38,10 +38,7 @@ namespace STOLON.CLI
         [Command("Print the STOLON version.")]
         public void Version() => Console.WriteLine(File.ReadAllText(".version"));
 
-        [Command("Set the STOLON version.", "set-version", needsDev: true)]
-        public void SetVersion()
-        {
-            File.ReadAllText(".version");
-        }
+        [Command("Set the STOLON version.", "version-set", needsDev: true)]
+        public void SetVersion(string newVer) => File.WriteAllText(Path.Combine(CLI.SourcePostBuildPath!, ".version"), newVer);
     }
 }
