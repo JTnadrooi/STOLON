621a1b1f495a25c0cf72b003161a99891d1a16e5
JTnadrooi <jtnadrooi@gmail.com>
2025-10-19 21:13:52 +0200

    Add static Version properties

--- src/STOLON.CLI/CommandProviders/GameCommandProvider.cs
+++ src/STOLON.CLI/CommandProviders/GameCommandProvider.cs
@@ -36,7 +36,7 @@ namespace STOLON.CLI
         }
 
         [Command("Print the STOLON version.")]
-        public void Version() => Console.WriteLine(File.ReadAllText(".version"));
+        public void Version() => Console.WriteLine(CLI.GameVersion);
 
         [Command("Set the STOLON version.", "version-set", needsDev: true)]
         public void SetVersion(string newVer) => File.WriteAllText(Path.Combine(CLI.SourcePostBuildPath!, ".version"), newVer);
