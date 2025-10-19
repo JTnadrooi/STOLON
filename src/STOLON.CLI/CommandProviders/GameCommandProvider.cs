88571454ece89ba8e74791939ea973ee2767fb7c
JTnadrooi <jtnadrooi@gmail.com>
2025-10-19 20:13:23 +0200

    Add "set-version" command

--- src/STOLON.CLI/CommandProviders/GameCommandProvider.cs
+++ src/STOLON.CLI/CommandProviders/GameCommandProvider.cs
@@ -10,12 +10,14 @@ namespace STOLON.CLI
     public class GameCommandProvider : CommandProvider
     {
         public GameCommandProvider() : base("sl") { }
+
         [Command("Start STOLON.")]
         public void Start()
         {
             using Process p = Process.Start("STOLON.exe");
             CLI.Debug.Log($"started STOLON as '{p.ProcessName}'.");
         }
+
         [Command("Exit STOLON.")]
         public void Exit()
         {
@@ -32,7 +34,11 @@ namespace STOLON.CLI
                 }
             else Console.WriteLine("No running STOLON processes found.");
         }
+
         [Command("Print the STOLON version.")]
         public void Version() => Console.WriteLine(File.ReadAllText(".version"));
+
+        [Command("Set the STOLON version.", "set-version")]
+        public void SetVersion() => File.ReadAllText(".version");
     }
 }
