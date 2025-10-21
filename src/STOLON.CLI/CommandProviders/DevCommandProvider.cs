0e94379d74f73e453f5219d7bca1854154405d40
JTnadrooi <jtnadrooi@gmail.com>
2025-10-21 16:08:04 +0200

    Unnested SourceCommandProvider

--- src/STOLON.CLI/CommandProviders/DevCommandProvider.cs
+++ src/STOLON.CLI/CommandProviders/DevCommandProvider.cs
@@ -16,28 +16,5 @@ namespace STOLON.CLI.CommandProviders
         {
             Console.WriteLine(CLI.IsDev);
         }
-
-        public class SourceCommandProvider : CommandProvider
-        {
-            public SourceCommandProvider() : base("src") { }
-
-            [Command("Open the local source code directory.", needsDev: true)]
-            public void _M()
-            {
-                using Process p = Process.Start(Environment.OSVersion.Platform switch
-                {
-                    PlatformID.Win32NT => "explorer.exe",
-                    PlatformID.Unix => "xdg-open",
-                    _ => "open"
-                }, CLI.SourcePath!);
-                CLI.Debug.Log("Opened source directory.");
-            }
-
-            [Command("Prints the local source code directory path.", needsDev: true)]
-            public void Path()
-            {
-                Console.WriteLine(CLI.SourcePath);
-            }
-        }
     }
 }
---
+++ src/STOLON.CLI/CommandProviders/SourceCommandProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STOLON.CLI
+{
+    public class SourceCommandProvider : CommandProvider
+    {
+        public SourceCommandProvider() : base("src") { }
+
+        [Command("Open the local source code directory.", needsDev: true)]
+        public void _M()
+        {
+            using Process p = Process.Start(Environment.OSVersion.Platform switch
+            {
+                PlatformID.Win32NT => "explorer.exe",
+                PlatformID.Unix => "xdg-open",
+                _ => "open"
+            }, CLI.SourcePath!);
+            CLI.Debug.Log("Opened source directory.");
+        }
+
+        [Command("Prints the local source code directory path.", needsDev: true)]
+        public void Path()
+        {
+            Console.WriteLine(CLI.SourcePath);
+        }
+    }
+}
