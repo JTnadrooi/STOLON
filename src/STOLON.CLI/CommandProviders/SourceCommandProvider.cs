48c7109f62e0fd1310fdf4845a616041c5ea9b8e
JTnadrooi <jtnadrooi@gmail.com>
2025-10-24 19:56:42 +0200

    Add support for subdirectory in src command

--- src/STOLON.CLI/CommandProviders/SourceCommandProvider.cs
+++ src/STOLON.CLI/CommandProviders/SourceCommandProvider.cs
@@ -12,15 +12,21 @@ namespace STOLON.CLI
         public SourceCommandProvider() : base("src") { }
 
         [Command("Open the local source code directory.", needsDev: true)]
-        public void _M()
+        public void _M(string? subDir = null)
         {
             using Process p = Process.Start(Environment.OSVersion.Platform switch
             {
                 PlatformID.Win32NT => "explorer.exe",
                 PlatformID.Unix => "xdg-open",
                 _ => "open"
-            }, CLI.SourcePath!);
-            CLI.Debug.Log("Opened source directory.");
+            }, CLI.SourcePath! + subDir switch
+            {
+                "sl" => "\\STOLON",
+                "cli" => "\\STOLON.CLI",
+                null => string.Empty,
+                _ => throw new ArgumentException("Accepted values; sl, cli", nameof(subDir))
+            });
+            CLI.Debug.Log($"Opened {(subDir == null ? string.Empty : $"'{subDir}'")} source directory.");
         }
 
         [Command("Prints the local source code directory path.", needsDev: true)]
