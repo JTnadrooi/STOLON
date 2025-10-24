f8c3cfa1d3c23364ac124e6c2db6291171647fc8
JTnadrooi <jtnadrooi@gmail.com>
2025-10-24 20:13:08 +0200

    Add support for mgfxc /Debug flag

--- src/STOLON.CLI/CommandProviders/BuildCommandProvider.cs
+++ src/STOLON.CLI/CommandProviders/BuildCommandProvider.cs
@@ -19,27 +19,31 @@ namespace STOLON.CLI
         }
 
         [Command("Build content.")]
-        public void _M()
+        public void _M(bool debug = false)
         {
-            Effects();
+            Effects(debug);
         }
 
         [Command("Compile effects.", idOverride: "fx", needsDev: true)]
-        public void Effects()
+        public void Effects(bool debug = false)
         {
             string effectsSource = CLI.SourcePath! + @"\STOLON\Content\Effects";
-            Directory.CreateDirectory("Effects\\");
             string[] effects = Directory.GetFiles(effectsSource, "*.fx");
+
             CLI.Debug.Log($">building effects from: '{effectsSource}'.");
+            CLI.Debug.Log($"found {effects.Length} files.");
+            Directory.CreateDirectory("Effects\\");
 
             for (int i = 0; i < effects.Length; i++)
             {
                 string effect = "Effects\\" + effects[i][(effectsSource.Length + 1)..];
                 string target = "Effects\\" + effects[i][(effectsSource.Length + 1)..^(".fx".Length)] + ".mgfx";
-                CLI.Debug.Log($"found '{effect}', compiling as '{target}'.");
+                CLI.Debug.Log($">compiling '{effect}' as '{target}'.");
 
-                using Process p = Process.Start("powershell.exe", $"dotnet tool run mgfxc {effects[i]} {target} /Profile:OpenGL");
+                using Process p = Process.Start("powershell.exe", $"dotnet tool run mgfxc {effects[i]} {target} /Profile:OpenGL" + (debug ? " /Debug" : string.Empty));
                 p.WaitForExit();
+
+                CLI.Debug.Success();
             }
 
             CLI.Debug.Success();
