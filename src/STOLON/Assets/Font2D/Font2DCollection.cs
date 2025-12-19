e09e013aa7c2283846ac77004a886531f59b5af0
JTnadrooi <jtnadrooi@gmail.com>
2025-12-19 08:45:38 +0100

    Add custom "Welcome" font as new Medium font

--- src/STOLON/Assets/Font2D/Font2DCollection.cs
+++ src/STOLON/Assets/Font2D/Font2DCollection.cs
@@ -28,7 +28,7 @@ namespace STOLON
     public class Font2DCollection : ResourceCollection<Font2D>
     {
         public Font2D Small => this["smollerMono"];
-        public Font2D Medium => this["pixeloid"];
+        public Font2D Medium => this["welcome"];
 
         public Font2DCollection() : base(new Font2DResourceLoader()) { }
     }
