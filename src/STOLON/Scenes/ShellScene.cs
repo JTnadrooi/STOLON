b13b46ddbb60bc7cc7b9049b8cd16fe0f370f5fd
JTnadrooi <jtnadrooi@gmail.com>
2025-12-20 19:59:17 +0100

    Add simple text editing (and text pos from mouse pos logic)

--- src/STOLON/Scenes/ShellScene.cs
+++ src/STOLON/Scenes/ShellScene.cs
@@ -11,17 +11,16 @@ namespace STOLON.Scenes
     {
         public Shell Shell { get; }
 
-        private Texture2D _testProfile;
-
         public ShellScene() : base("shell")
         {
-            Shell = new Shell();
-            Shell.WriteLine("Hello world!\nWhat a lovelly day.\n> _");
-            Shell.WriteLine("^[s]initialising environment..\r\n    ^---searching for entities..\r\n        ^---called assembly scan for type 'STOLON.Entity\".\r\n        ^---found entity with id 'deceit\" and name 'Deceit\".\r\n        ^---found entity with id 'goldsilk\" and name 'Goldsilk\".\r\n        ^---found entity with id 'north\" and name 'North\".\r\n        ^---found entity with id 'silo\" and name 'Silo\".\r\n        ^---success.\r\n    ^[s]contructing stolon ui..\r\n        ^---success.: time taken: 1ms.\r\n    ^---searching for overlays..\r\n        ^---called assembly scan for type 'STOLON.IOverlay\".\r\n        ^---found overlay with id 'loading\".\r\n        ^---adding overlay of id loading..\r\n            ^---success.\r\n        ^---found overlay with id 'transition_dither\".\r\n        ^---adding overlay of id transition_dither..\r\n            ^---success.\r\n        ^---found overlay with id 'transition\".\r\n        ^---adding overlay of id transition..\r\n            ^---success.\r\n        ^---success.\r\n    ^---success.: time taken: 16ms.\r\n^---success.: time taken: 261ms.".Replace("\r", ""));
-            //Shell.WriteLine("> hello \nworld");
             STOLON.UI.Textframe.Hide = true;
-            _testProfile = STOLON.Textures["Entities\\north\\north-128"];
-            //Shell.WriteLine("[envi] hello world");
+
+            Shell = new Shell();
+            //Shell.WriteLine("Hello world!\nWhat a lovelly day.\n> _");
+            //Shell.WriteLine("^[s]initialising environment..\r\n    ^---searching for entities..\r\n        ^---called assembly scan for type 'STOLON.Entity\".\r\n        ^---found entity with id 'deceit\" and name 'Deceit\".\r\n        ^---found entity with id 'goldsilk\" and name 'Goldsilk\".\r\n        ^---found entity with id 'north\" and name 'North\".\r\n        ^---found entity with id 'silo\" and name 'Silo\".\r\n        ^---success.\r\n    ^[s]contructing stolon ui..\r\n        ^---success.: time taken: 1ms.\r\n    ^---searching for overlays..\r\n        ^---called assembly scan for type 'STOLON.IOverlay\".\r\n        ^---found overlay with id 'loading\".\r\n        ^---adding overlay of id loading..\r\n            ^---success.\r\n        ^---found overlay with id 'transition_dither\".\r\n        ^---adding overlay of id transition_dither..\r\n            ^---success.\r\n        ^---found overlay with id 'transition\".\r\n        ^---adding overlay of id transition..\r\n            ^---success.\r\n        ^---success.\r\n    ^---success.: time taken: 16ms.\r\n^---success.: time taken: 261ms.".Replace("\r", ""));
+
+            Shell.WriteLine("Hello. This is a long first line no?");
+            Shell.WriteLine("This is on the second line.");
         }
 
         protected override void UpdateUI(int elapsedMilliseconds)
@@ -31,7 +30,7 @@ namespace STOLON.Scenes
 
         public override void Draw(DrawingContext drawingContext)
         {
-            drawingContext.Draw(_testProfile, new Microsoft.Xna.Framework.Vector2(500, 200));
+            drawingContext.Draw(STOLON.Textures["Entities\\north\\north-128"], new Microsoft.Xna.Framework.Vector2(500, 200));
             Shell.Draw(drawingContext);
         }
     }
--- src/STOLON/Shell/Shell.cs
+++ src/STOLON/Shell/Shell.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,29 +11,105 @@ namespace STOLON
     public class Shell : Service
     {
         private string _text;
-
         private Font2D _font;
+        private Vector2 _textScale;
+        private Vector2 _debugCursorPos;
+        private Vector2 _textPos;
+
+        private const string NewLine = "\n";
+
+        public bool IsFocus { get; set; }
 
         public Shell() : base(STOLON.Environment)
         {
             _text = string.Empty;
-            _font = STOLON.Fonts.Small;
+            _font = STOLON.Fonts.Medium;
+            _textScale = Vector2.One;
+            _debugCursorPos = Vector2.Zero;
+
+            STOLON.Instance.Window.TextInput += OnTextInput;
+
+            IsFocus = true;
         }
 
         public void WriteLine(string text)
         {
-            _text = text; // ADD FONT SYSTEM THAT ALWAYS DRAWS IT NORMALLY
+            Write(text + NewLine);
+        }
+
+        public void Write(char character) => Write(character.ToString());
+        public void Write(string text)
+        {
+            _text += text;
         }
 
         public override void Update(int elapsedMilliseconds)
         {
+            _textScale = Vector2.One;
+            int lines = _text.Count(c => c == '\n') + 1;
+            _textPos = new Vector2(10, STOLON.V_HEIGHT - _font.Dimensions.Y - _font.Dimensions.Y * lines);
 
+
+            if (STOLON.Input.IsClicked(MouseButton.Left))
+            {
+                int index = GetCharacterIndexAt(STOLON.Input.VirtualMousePos);
+
+                Console.WriteLine(index);
+
+                if (index != -1) _text = ReplaceAt(_text, index, '_');
+            }
+        }
+
+        private int GetCharacterIndexAt(Vector2 pos)
+        {
+            int charWidth = (int)_font.Dimensions.X;
+            //int charHeight = (int)_font.Dimensions.Y;
+
+            int result = (int)((pos.X - _textPos.X) / charWidth);
+
+            string firstLine = _text.Split(NewLine).First();
+
+            if (result > firstLine.Length) return -1;
+
+            return result;
+        }
+
+        private void OnTextInput(object? sender, TextInputEventArgs e)
+        {
+            switch (e.Character)
+            {
+                case '\b':
+                    _text = _text[..^1];
+                    break;
+                case '\r':
+                    Write(NewLine);
+                    return;
+                default:
+                    Write(e.Character);
+                    break;
+            }
         }
 
         public override void Draw(DrawingContext drawingContext)
         {
-            int lines = _text.Count(c => c == '\n') + 1;
-            drawingContext.DrawString(_font, _text, new Vector2(10, STOLON.V_HEIGHT - _font.Dimensions.Y - _font.Dimensions.Y * lines));
+            drawingContext.DrawString(_font, _text, _textPos, scale: _textScale);
+            drawingContext.DrawLine(STOLON.Input.VirtualMousePos, STOLON.Input.VirtualMousePos);
         }
+
+        #region TEMP_UTILS
+
+
+        private static string ReplaceAt(string input, int index, char newChar)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            char[] chars = input.ToCharArray();
+            chars[index] = newChar;
+            return new string(chars);
+        }
+
+        #endregion
     }
 }
