aabfcdfd2ecbfea0941833a5f979d6d7e72bc4a3
JTnadrooi <jtnadrooi@gmail.com>
2026-01-07 17:24:28 +0100

    Remove Entity register logic (Autofac does it already)

--- src/STOLON/Environment.cs
+++ src/STOLON/Environment.cs
@@ -5,6 +5,13 @@
     /// </summary>
     public class Environment : Service, IDialogueProvider, ISingletonDependency
     {
+        private readonly IRichLogger _logger;
+        private readonly IAudioEngine _audioEngine;
+        private readonly ISceneManager _sceneManager;
+        private readonly IOverlayManager _overlayManager;
+        private readonly Interface _ui;
+        private readonly IEnumerable<Entity> _entities;
+
         /// <summary>
         /// A <see cref="Dictionary{TKey, TValue}"/> listing all <see cref="Entity"/> objects and their <see cref="Entity.Id"/>.
         /// </summary>
@@ -13,13 +20,6 @@
         public string Name => "Environment";
 
         private Dictionary<string, Entity> _entityDict;
-
-        private readonly IRichLogger _logger;
-        private readonly IAudioEngine _audioEngine;
-        private readonly ISceneManager _sceneManager;
-        private readonly IOverlayManager _overlayManager;
-        private readonly Interface _ui;
-        private readonly IEnumerable<Entity> _entities;
 
         public Environment(IRichLogger logger, IAudioEngine audioEngine, ISceneManager sceneManager, Interface ui, IOverlayManager overlayManager, IEnumerable<Entity> entities) : base(null)
         {
@@ -33,16 +33,15 @@
             _entityDict = new Dictionary<string, Entity>();
             _sceneManager = sceneManager;
         }
+
         public void Initialize()
         {
             _logger.Log(">[s]initialising environment");
-            _logger.Log(">searching for entities");
             foreach (Entity entity in _entities)
             {
-                _logger.Log($"found entity with id '{entity.Id}\" and name '{entity.Name}\".");
-                RegisterEntity(entity);
+                _entityDict.Add(entity.Id, entity);
+                _logger.Log($"registered entity with id '{entity.Id}\" and name '{entity.Name}\".");
             }
-            _logger.Success();
 
             _sceneManager.ChangeScene<MenuScene>();
 
@@ -52,19 +51,20 @@
             //));
             _logger.Success();
         }
+
         public override void Update(int elapsedMilliseconds)
         {
             _ui.Update(elapsedMilliseconds);
 
             _sceneManager.Update(elapsedMilliseconds);
 
-            //_userInterface.PostUpdate(elapsedMilliseconds);
             _audioEngine.Update(elapsedMilliseconds);
             //STOLON.Instance.DRP.UpdateDetails(STOLON.SceneManager.Current.DRPStatus);
 
             _overlayManager.Update(elapsedMilliseconds);
             base.Update(elapsedMilliseconds);
         }
+
         public override void Draw(DrawingContext drawingContext)
         {
             _sceneManager.Draw(drawingContext);
@@ -73,24 +73,5 @@
             _overlayManager.Draw(drawingContext);
             base.Draw(drawingContext);
         }
-
-        /// <summary>
-        /// Register a new <see cref="Entity"/>.
-        /// </summary>
-        /// <param name="entity">The entity to register.</param>
-        public void RegisterEntity(Entity entity)
-        {
-            _entityDict.Add(entity.Id, entity);
-        }
-        /// <summary>
-        /// Deregister a new <see cref="Entity"/>. <strong>Should never be used.</strong>
-        /// </summary>
-        /// <param name="entity">The entity to deregister.</param>
-        public void DeregisterEntity(string characterId)
-        {
-            _entityDict.Remove(characterId);
-        }
-
-        public Entity GetEntityInstance<TEntity>() => Entities.First(kvp => kvp.Value is TEntity).Value;
     }
 }
