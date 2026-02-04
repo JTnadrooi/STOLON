d75b587e893e98332d22441a946e162c46eddddc
JTnadrooi <jtnadrooi@gmail.com>
2026-02-04 11:20:10 +0100

    Remove ShellScene.Shell property, use DI instead

--- src/STOLON/Scenes/ShellScene.cs
+++ src/STOLON/Scenes/ShellScene.cs
@@ -4,8 +4,6 @@ namespace STOLON
 {
     public class ShellScene : Scene
     {
-        public Shell Shell => _shell;
-
         private readonly ITexture2DCollection _textures;
         private readonly IRichLogger _logger;
         private readonly Interface _ui;
@@ -27,30 +25,30 @@ namespace STOLON
             //Shell.WriteLine("Hello world!\nWhat a lovelly day.\n> _");
             //Shell.WriteLine("^[s]initialising environment..\r\n    ^---searching for entities..\r\n        ^---called assembly scan for type 'STOLON.Entity\".\r\n        ^---found entity with id 'deceit\" and name 'Deceit\".\r\n        ^---found entity with id 'goldsilk\" and name 'Goldsilk\".\r\n        ^---found entity with id 'north\" and name 'North\".\r\n        ^---found entity with id 'silo\" and name 'Silo\".\r\n        ^---success.\r\n    ^[s]contructing stolon ui..\r\n        ^---success.: time taken: 1ms.\r\n    ^---searching for overlays..\r\n        ^---called assembly scan for type 'STOLON.IOverlay\".\r\n        ^---found overlay with id 'loading\".\r\n        ^---adding overlay of id loading..\r\n            ^---success.\r\n        ^---found overlay with id 'transition_dither\".\r\n        ^---adding overlay of id transition_dither..\r\n            ^---success.\r\n        ^---found overlay with id 'transition\".\r\n        ^---adding overlay of id transition..\r\n            ^---success.\r\n        ^---success.\r\n    ^---success.: time taken: 16ms.\r\n^---success.: time taken: 261ms.".Replace("\r", ""));
 
-            Shell.WriteLine("Hello. This is a long first line no?");
-            Shell.WriteLine("This is on the second line.");
-            Shell.WriteTexture(_textures["Entities\\north\\north-128"]);
-            Shell.HasInputLine = true;
-            Shell.WriteLine("This is on the THIRD line!");
-            Shell.WriteLine("This is on the FOURTH line! (It can't get any crazier than this!1!)");
-            Shell.WriteTexture(_textures["Entities\\north\\north-128"]);
+            _shell.WriteLine("Hello. This is a long first line no?");
+            _shell.WriteLine("This is on the second line.");
+            _shell.WriteTexture(_textures["Entities\\north\\north-128"]);
+            _shell.HasInputLine = true;
+            _shell.WriteLine("This is on the THIRD line!");
+            _shell.WriteLine("This is on the FOURTH line! (It can't get any crazier than this!1!)");
+            _shell.WriteTexture(_textures["Entities\\north\\north-128"]);
         }
 
         protected override void UpdateUI(int elapsedMilliseconds)
         {
             if (_input.IsPressed(Keys.LeftControl))
             {
-                if (_input.IsClicked(Keys.W)) Shell.WriteLine(new Random().Next().ToString());
-                if (_input.IsClicked(Keys.E)) Shell.Write(new Random().Next().ToString());
+                if (_input.IsClicked(Keys.W)) _shell.WriteLine(new Random().Next().ToString());
+                if (_input.IsClicked(Keys.E)) _shell.Write(new Random().Next().ToString());
             }
 
-            Shell.Update(elapsedMilliseconds);
+            _shell.Update(elapsedMilliseconds);
         }
 
         public override void Draw(DrawingContext drawingContext)
         {
-            drawingContext.Draw(_textures["Entities\\north\\north-128"], new Microsoft.Xna.Framework.Vector2(500, 200));
-            Shell.Draw(drawingContext);
+            drawingContext.Draw(_textures["Entities\\north\\north-128"], new Vector2(500, 200));
+            _shell.Draw(drawingContext);
         }
     }
 }
