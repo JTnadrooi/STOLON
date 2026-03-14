21729c43cee004911751bd83a1a372c814f91458
JTnadrooi <jtnadrooi@gmail.com>
2026-03-14 11:33:43 +0100

    Split command providers

---
+++ src/STOLON/Kernel/CommandProviders/AddressCommandProvider.cs
@@ -0,0 +1,48 @@
+﻿using AsitLib.CommandLine;
+
+namespace STOLON
+{
+    public sealed class InitAddressCommandFlag : CommandFlag
+    {
+        public HashSet<string> Selection { get; } = new HashSet<string>();
+
+        public string Address { get; set; }
+
+        public InitAddressCommandFlag(string address)
+        {
+            Address = address;
+        }
+    }
+
+    public class AddressCommandProvider : CommandProvider
+    {
+        private readonly CommandManager _commandManager;
+        private readonly Shell _shell;
+        private readonly Entity[] _entities;
+
+        public AddressCommandProvider(CommandManager commandManager, Shell shell, Entity[] entities) : base("addr") // way too long id but it shoulden't be typed by the user.
+        {
+            _commandManager = commandManager;
+            _shell = shell;
+            _entities = entities;
+        }
+
+        [KernelCommand("Initialize an adress.", Id = "addr")]
+        public void InitializeAddress(string id)
+        {
+            InitAddressCommandFlag flag = _commandManager.SetFlag(new InitAddressCommandFlag(id));
+        }
+
+        [KernelCommand("Select a .", Id = "selc", RequiredFlag = typeof(InitAddressCommandFlag))]
+        public void SelectEntity(string entityName)
+        {
+            InitAddressCommandFlag flag = (InitAddressCommandFlag)_commandManager.ActiveFlag!;
+
+            flag.Selection.Add(entityName);
+
+            _shell.WriteLine(flag.Selection.ToJoinedString(", "));
+
+            _shell.WriteTexture(_entities.First(e => e.Id == entityName).Mipmaps[128]);
+        }
+    }
+}
--- src/STOLON/Kernel/CommandProviders/GenericCommandProvider.cs
+++ src/STOLON/Kernel/CommandProviders/GenericCommandProvider.cs
@@ -2,29 +2,24 @@
 
 namespace STOLON
 {
-    public sealed class InitAddressCommandFlag : CommandFlag
-    {
-
-    }
-
     public class GenericCommandProvider : CommandProvider
     {
         private readonly CommandManager _commandManager;
         private readonly Shell _shell;
 
-        public GenericCommandProvider(Shell shell, CommandManager commandManager) : base("generic") // way too long id but it shoulden't be typed by the user.
+        public GenericCommandProvider(CommandManager commandManager, Shell shell) : base("generic") // way too long id but it shoulden't be typed by the user.
         {
             _commandManager = commandManager;
             _shell = shell;
         }
 
-        [KernelCommand("Prints the reality number.", Aliases = ["v"], Id = "version", IsGenericFlag = true)] // not the game version, lore version.
-        public void GetVersion() => _shell.WriteLine($"revise--1236979249_sto&a");
+        [KernelCommand("Prints the reality version.", Aliases = ["v"], Id = "version", IsGenericFlag = true)] // not the game version, lore version.
+        public void GetVersion() => _shell.WriteLine($"revise--1236979249_sto+a");
 
-        [KernelCommand(".", Id = "addr")]
-        public void InitAddress() => _commandManager.SetFlag<InitAddressCommandFlag>();
-
-        [KernelCommand(".", Id = "flag", RequiredFlag = typeof(InitAddressCommandFlag))]
-        public void GetActiveFlag() => _shell.WriteLine(_commandManager.ActiveFlag);
+        [KernelCommand(".", Id = "flag", RequiredFlag = typeof(InitAddressCommandFlag), IsExternal = true)]
+        public void GetActiveFlag()
+        {
+            _shell.WriteLine(_commandManager.ActiveFlag);
+        }
     }
 }
