1af35ec26869998eb750d4317d2042315664e653
JTnadrooi <jtnadrooi@gmail.com>
2026-03-21 20:06:55 +0100

    Refactor GetRandomVector() extension method as NextVector2()

--- src/STOLON/Extensions/RandomExtensions.cs
+++ src/STOLON/Extensions/RandomExtensions.cs
@@ -8,11 +8,11 @@ namespace STOLON.Extensions
 {
     public static class RandomExtensions
     {
-        public static Vector2 GetRandomVector(this Random random, Vector2? max = null, Vector2? minSize = null)
+        public static Vector2 NextVector2(this Random random, Vector2? max = null, Vector2? min = null)
         {
             max ??= new Vector2(float.MaxValue, float.MaxValue);
-            minSize ??= Vector2.Zero;
-            return new Vector2(random.Next((int)minSize.Value.X, (int)max.Value.X), random.Next((int)minSize.Value.Y, (int)max.Value.Y));
+            min ??= new Vector2(float.MinValue, float.MinValue);
+            return new Vector2(random.Next((int)min.Value.X, (int)max.Value.X), random.Next((int)min.Value.Y, (int)max.Value.Y));
         }
     }
 }
