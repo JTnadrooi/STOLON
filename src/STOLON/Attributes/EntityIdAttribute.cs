7d0a759a244a09ed73221a03d3ce9114d2276eb4
JTnadrooi <jtnadrooi@gmail.com>
2026-03-23 20:04:34 +0100

    Add EntityIdAttribute ValidationAttribute

---
+++ src/STOLON/Attributes/EntityIdAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STOLON
+{
+    public sealed class EntityIdAttribute : ValidationAttribute
+    {
+        private static readonly Entity[]? s_entities;
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (s_entities is null) return ValidationResult.Success;
+
+            string input = value.ToString()!;
+            bool exists = s_entities.Any(e => e.Id == input);
+            return exists ? ValidationResult.Success : new ValidationResult($"Entity '{input}' does not exist.");
+        }
+
+        static EntityIdAttribute()
+        {
+            if (STOLON.IsInitiated)
+            {
+                s_entities = STOLON.Services.Resolve<Entity[]>();
+            }
+        }
+    }
+}
