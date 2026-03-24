c4fdcaebf604ceb165422bb8667787eaa7e1b0a2
JTnadrooi <jtnadrooi@gmail.com>
2026-03-24 11:36:07 +0100

    Implement IReadOnlyDictionary and IReadOnlyList and add Add/Remove-Range methods to EntitySelection

--- src/STOLON/Entities/EntitySelection.cs
+++ src/STOLON/Entities/EntitySelection.cs
@@ -1,4 +1,6 @@
-﻿namespace STOLON
+﻿using System.Collections;
+
+namespace STOLON
 {
     public readonly struct SelectionEntry
     {
@@ -19,13 +21,15 @@
         public override string ToString() => $"Entity: {Entity.Id}, Allocation: {Allocation}, VAllocation: {(IsPostAllocation ? VAllocation : "<n/a>")}";
     }
 
-    public sealed class EntitySelection
+    public sealed class EntitySelection : IReadOnlyDictionary<string, SelectionEntry>, IReadOnlyList<SelectionEntry>
     {
         public ReadOnlyDictionary<string, SelectionEntry> Entries { get; }
         public int TotalVAllocation => IsPostAllocation ? _totalVAllocation : throw new InvalidOperationException();
         public bool IsPostAllocation { get; private set; }
         public int Count => Entries.Count;
         public int MaxEntries { get; }
+        public IEnumerable<string> Keys => Entries.Keys;
+        public IEnumerable<SelectionEntry> Values => Entries.Values;
 
         private readonly Dictionary<string, SelectionEntry> _entries;
         private readonly List<Entity> _selectedEntities;
@@ -46,6 +50,28 @@
             _totalVAllocation = 0;
             IsPostAllocation = true;
             MaxEntries = maxEntries;
+        }
+
+        private void RecalculateAllocations()
+        {
+            IsPostAllocation = false;
+
+            _entries.Clear();
+            for (int i = 0; i < _selectedEntities.Count; i++) // create source without virtual values.
+                _entries.Add(_selectedEntities[i].Id, new SelectionEntry(_selectedEntities[i], 100 / _selectedEntities.Count));
+
+            Stack<SelectionEntry> entryBuffer = new Stack<SelectionEntry>(MaxEntries);
+            for (int i = 0; i < _selectedEntities.Count; i++) // create entryBuffer with virtual values. (using _entries referenced in GetVirtualAllocation())
+                entryBuffer.Push(new SelectionEntry(_selectedEntities[i], 100 / _selectedEntities.Count, _selectedEntities[i].GetVirtualAllocation(this)));
+
+            _entries.Clear();
+            for (int i = 0; i < _selectedEntities.Count; i++) // make buffer the new source without ref change.
+                _entries.Add(entryBuffer.Peek().Entity.Id, entryBuffer.Pop());
+
+            IsPostAllocation = true;
+
+            _totalVAllocation = _entries.Sum(e => e.Value.VAllocation);
+            if (_totalVAllocation == 99) _totalVAllocation = 100;
         }
 
         public bool Add(string id)
@@ -70,35 +96,51 @@
             return true;
         }
 
+        public bool AddRange(params IEnumerable<string> ids)
+        {
+            bool allSucceeded = true;
+
+            foreach (string id in ids)
+            {
+                if (!Add(id))
+                {
+                    allSucceeded = false;
+                }
+            }
+
+            return allSucceeded;
+        }
+
+        public bool RemoveRange(params IEnumerable<string> ids)
+        {
+            bool allSucceeded = true;
+
+            foreach (string id in ids)
+            {
+                if (!Remove(id))
+                {
+                    allSucceeded = false;
+                }
+            }
+
+            return allSucceeded;
+        }
+
         public bool Contains(string id)
         {
             return Entries.ContainsKey(id);
         }
 
         public int GetSlot(string id) => _selectedEntities.GetFirstIndexWhere(e => e.Id == id);
-        private void RecalculateAllocations()
-        {
-            IsPostAllocation = false;
 
-            _entries.Clear();
-            for (int i = 0; i < _selectedEntities.Count; i++) // create source without virtual values.
-                _entries.Add(_selectedEntities[i].Id, new SelectionEntry(_selectedEntities[i], 100 / _selectedEntities.Count));
-
-            Stack<SelectionEntry> entryBuffer = new Stack<SelectionEntry>(MaxEntries);
-            for (int i = 0; i < _selectedEntities.Count; i++) // create entryBuffer with virtual values. (using _entries referenced in GetVirtualAllocation())
-                entryBuffer.Push(new SelectionEntry(_selectedEntities[i], 100 / _selectedEntities.Count, _selectedEntities[i].GetVirtualAllocation(this)));
-
-            _entries.Clear();
-            for (int i = 0; i < _selectedEntities.Count; i++) // make buffer the new source without ref change.
-                _entries.Add(entryBuffer.Peek().Entity.Id, entryBuffer.Pop());
-
-            IsPostAllocation = true;
-
-            _totalVAllocation = _entries.Sum(e => e.Value.VAllocation);
-            if (_totalVAllocation == 99) _totalVAllocation = 100;
-        }
         public int GetAllocation(string id) => _entries.TryGetValue(id, out SelectionEntry entry) ? entry.Allocation : 0;
         public int GetVirtualAllocation(string id) => IsPostAllocation ? (_entries.TryGetValue(id, out SelectionEntry entry) ? entry.VAllocation : 0) : throw new InvalidOperationException();
+
+        public bool ContainsKey(string key) => Contains(key);
+        public bool TryGetValue(string key, out SelectionEntry value) => _entries.TryGetValue(key, out value);
+        public IEnumerator<KeyValuePair<string, SelectionEntry>> GetEnumerator() => _entries.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_entries).GetEnumerator();
+        IEnumerator<SelectionEntry> IEnumerable<SelectionEntry>.GetEnumerator() => _entries.Values.GetEnumerator();
 
         public override string ToString() => $"IsPostAllocation: {IsPostAllocation}, TotalVAllocation: {(IsPostAllocation ? TotalVAllocation : "<n/a>")}, Entries: [{string.Join(", ", Entries.Values.Select(e => e.ToString()))}]";
     }
