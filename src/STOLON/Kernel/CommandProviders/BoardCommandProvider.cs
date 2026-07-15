dd3c3aa8775c64872aecbf993a5f515ea59f563c
JTnadrooi <jtnadrooi@gmail.com>
2026-07-15 12:10:42 +0200

    Rename place-at alias "pat" to "@"

--- src/STOLON/Kernel/CommandProviders/BoardCommandProvider.cs
+++ src/STOLON/Kernel/CommandProviders/BoardCommandProvider.cs
@@ -1,4 +1,4 @@
-using AsitLib.CommandLine;
+﻿using AsitLib.CommandLine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,35 +16,18 @@ namespace STOLON
             _commandManager = commandManager;
         }
 
-        private Point GetPointFromCoords(string coords) // ex: a1
-        {
-            if (string.IsNullOrWhiteSpace(coords) || coords.Length != 2)
-                throw new CommandArgumentException("Invalid coordinates");
-
-            char file = char.ToLower(coords[0]);
-            char rank = coords[1];
-
-            if (file < 'a' || file > 'h' || rank < '1' || rank > '8')
-                throw new CommandArgumentException("Invalid coordinates");
-
-            int x = file - 'a';
-            int y = rank - '1';
-
-            return new Point(x, y);
-        }
-
-        [KernelCommand("Places a token at the specified position.", Aliases = ["pat"], RequiredFlag = typeof(BoardCommandFlag))]
+        [KernelCommand("Places a token at the specified position.", Aliases = ["@"], RequiredFlag = typeof(BoardCommandFlag))]
         public void PlaceAt(string coords)
         {
             BoardCommandFlag flag = (BoardCommandFlag)_commandManager.ActiveFlag!;
 
-if (flag.IsCurrentEntityUserControlled)
+            if (flag.IsCurrentEntityUserControlled)
             {
-            Point movePos = Board.GetPointFromCoords(coords);
-            IMove move = new GravityAffectedMove(movePos.X, movePos.Y);
+                Point movePos = Board.GetPointFromCoords(coords);
+                IMove move = new GravityAffectedMove(movePos.X, movePos.Y);
 
-            move.Apply(flag.Board.State, flag.Board.State.CurrentEntity);
-}
+                move.Apply(flag.Board.State, flag.Board.State.CurrentEntity);
+            }
         }
     }
 }
